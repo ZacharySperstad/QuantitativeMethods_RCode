f4d6a3b7517f6e778a05b4c3cc25caa2b3192301
Zachary Sperstad <43994319+ZacharySperstad@users.noreply.github.com>
2018-11-11 16:14:34 -0600

    Add files via upload

--- Sperstad_Lab10.R
+++ Sperstad_Lab10.R
@@ -34,7 +34,7 @@ plot(nj.clust,main='Neighbor Joining Tree') #Plots the neighbor joining tree.
 
 ################### Exercise 2 ######################
 
-sexspp<-cbind(master$spp,master$sex)
+sexspp<-cbind(master$spp,master$sex) #Creates a new object with the species identity 
 table<-matrix(ncol=3,nrow=12) #Creates an empty matrix with 3 columns and 12 rows.
 
 for(i in 1:12){
@@ -43,7 +43,10 @@ for(i in 1:12){
                   unique(master$sex)[[j]])
     table[i,j]<-nrow(sub)
   }
-}
+} #This for loop is looking through the sexspp object and essentially tallying
+  #how many individuals are a given species AND sex.
+
+#Q3-
 
 apply(table,2,as.numeric) #Makes columns numerical variables.
 rownames(table)<-mean.cran$Group.1 #Applies species identities as row names.
@@ -51,4 +54,82 @@ sex<-c("f","m","u") #Creates a vector with sex identity abbreviations.
 colnames(table)<-sex #Assigns sex identities as column names.
 
 require(ca) #Allows ca functions to be performed.
-sexspp.ca<-ca(table)
+sexspp.ca<-ca(table) #Performs a correspondence analysis.
+summary(sexspp.ca) #Gives a summary of the correspondence analysis.
+#Principal inertias (eigenvalues):
+#dim    value      %   cum%   scree plot               
+#1      0.114761  83.6  83.6  *********************    
+#2      0.022445  16.4 100.0  ****                     
+#-------- -----                                 
+#Total: 0.137206 100.0                                 
+
+#Rows:
+#     name   mass  qlt  inr     k=1 cor ctr    k=2 cor ctr  
+#1  |   Cm |   16 1000    6 |   205 874   6 |   78 126   4 |
+#2  |   Gb |   27 1000   15 |   214 620  11 |  168 380  34 |
+#3  |   Gg |  130 1000   74 |   216 592  53 |  179 408 186 |
+#4  |   Ha |  101 1000   51 |   -68  67   4 |  256 933 293 |
+#5  |   Hm |  104 1000   58 |  -276 996  69 |  -18   4   2 |
+#6  |   Ho |  101 1000   28 |   194 993  33 |  -16   7   1 |
+#7  |   Hs |   69 1000  587 | -1076 999 701 |  -27   1   2 |
+#8  |   Pb |   31 1000   15 |   212 684  12 |  144 316  29 |
+#9  |   Ph |   20 1000    6 |   202 926   7 |   57  74   3 |
+#10 |   Po |  104 1000   30 |   190 929  33 |  -53  71  13 |
+#11 |   Pp |   88 1000   16 |  -146 852  16 |  -61 148  14 |
+#12 |   Pt |  208 1000  114 |   173 398  54 | -212 602 419 |
+  
+#Columns:
+#    name   mass  qlt  inr     k=1 cor ctr    k=2 cor ctr  
+#1 |    f |  475 1000   88 |    27  30   3 | -157 970 522 |
+#2 |    m |  488 1000  114 |   103 330  45 |  147 670 467 |
+#3 |    u |   37 1000  798 | -1729 998 952 |   84   2  12 |
+
+#Q4- Pt has the greatest mass.
+
+plot(sexspp.ca,mass=T,arrows=c(T,T), map='rowgreen')
+
+#Q5- Hs, Hm, Pp, and Ha all appear to be close to u, meaning the have individuals
+#    with unknown sexes. I doubled-checked this in the sexspp matrix and it is true
+#    (pretty neat). There seems to be an abundance of females in Pt and those with
+#    identifiable sexes in Pp also tend to be females. The Ha individuals that
+#    were able to be sexed were predominately male, as were the gorillas (Gg and Gb)
+#    and Pb (which I believe is the blue monkey).
+
+################### Exercise 2 ######################
+
+data("HairEyeColor")
+hec.mca<-mjca(HairEyeColor)
+summary(hec.mca)
+#dim    value      %   cum%   scree plot               
+#1      0.054579  65.6  65.6  **********************   
+#2      0.006263   7.5  73.1  ***                      
+#3      0.000871   1.0  74.1                           
+#-------- -----                                 
+#Total: 0.083229                                       
+
+
+#Columns:
+#           name   mass  qlt  inr    k=1 cor ctr    k=2 cor ctr  
+#1  | Hair:Black |   61  738  117 | -310 613 107 | -140 125 191 |
+#2  | Hair:Blond |   72  745  126 |  527 741 364 |  -41   5  19 |
+#3  | Hair:Brown |  161  691   71 |  -97 648  27 |   25  43  16 |
+#4  |   Hair:Red |   40  667  119 |  -84 111   5 |  187 556 224 |
+#5  |   Eye:Blue |  121  754  100 |  337 742 252 |  -42  12  35 |
+#6  |  Eye:Brown |  124  724   96 | -295 688 198 |  -68  37  92 |
+#7  |  Eye:Green |   36  677  121 |   86  95   5 |  214 582 262 |
+#8  |  Eye:Hazel |   52  739  114 | -141 453  19 |  112 286 105 |
+#9  | Sex:Female |  176  588   64 |   57 456  11 |  -31 132  27 |
+#10 |   Sex:Male |  157  588   72 |  -64 456  12 |   35 132  30 |
+
+#Q6- 65.6% of the total inertia in the dataset is explained by the first factor.
+
+plot(hec.mca,mass=T,arrows=c(T,T), map='rowgreen') #Plots the results of the MCA.
+
+#Q7- Okay...Here we go. People with black hair tend to have brown eyes (quadrent 3).
+#    Females tend to have blue eyes and blonde hair (quadrent 4). Males tend to
+#    have brown or red hair as well as hazel or green eyes. Ignoring sex, we could
+#    also say, in general, people with blue eyes tend to have blonde hair (or vice
+#    versa) with thos with brown or red hair tend to have green or hazel eyes (or
+#    vice versa). Because black hair and brown eyes seem to be equal distances
+#    away from females and males, I am going to say that these characters are
+#    evenly split between males and females.
